0e3dabce1f16db3bc6eddf2eb20d2da7ee73bf36
Александр Маслов <al.maslovrg@yandex.ru>
2020-02-27 19:41:29 +0300

    Добавьте файлы проекта.

---
+++ Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
